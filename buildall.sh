25e5fc75575d2a22040c7c7ffe286a9ada5ac9d5
David Joffe <david@djsoftware.tech>
2026-04-15 14:38:54 +0200

    Enhance buildall.sh with comments and notes
    Added comments and notes about the build process.

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# DJ Software Utils https://github.com/djsoftware1/djutils-workspace/
+# Build helper
+
+# This needs a litle refining, haha, but will do for now ...
+# One issue with things like this is if one build fails it could leave the current working directory "wrong" for the next step
+# That is not high priority ... can do later ...
 
 cd clipx && ./build.sh && cd ..
 cd for-each && ./build.sh && cd ..
